6c62279e672909effe2e0a7a6d453ebfab0cba09
Shivank Sriraj <shivanksriraj@gmail.com>
2020-08-18 16:42:45 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,11 +3,16 @@ function fetchTime() {
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
-    sec = (sec * 6) + 180;
-    min = (min * 6) + 180;
-    hour = (hour * 30) + 180;
-    document.querySelector(".second-hand").style.transform = "rotate("+sec+"deg)";
-    document.querySelector(".minute-hand").style.transform = "rotate("+min+"deg)";
-    document.querySelector(".hour-hand").style.transform = "rotate("+hour+"deg)";
+    sec = sec * 6;
+    min = min * 6;
+    hour = hour * 30;
+    document.querySelector(".second-hand").style.transform = "rotate(" + (sec + 180) + "deg)";
+    document.querySelector(".minute-hand").style.transform = "rotate(" + ((min + sec/60) + 180) + "deg)";
+    document.querySelector(".hour-hand").style.transform = "rotate(" + ((hour + min/12) + 180) + "deg)";
 }
 setInterval(fetchTime, 1000);
+
+/*
+360deg second -> 6deg min  =  60deg second -> 1deg min.
+360deg min -> 30deg hour  =  12deg min -> 1deg hour.
+*/
